e4c3749a36c29bb166399b2590aa1f71ff3a151f
ssmshsoil-ship-it <ssmshsoil@gmail.com>
2026-04-14 21:17:43 +0900

    Update transport_sw.js

--- transport_sw.js
+++ transport_sw.js
@@ -1,25 +1,21 @@
-const CACHE = 'transport-v1';
+const CACHE = 'transport-v2';
 // GitHub Pages용 상대 경로('./') 적용 및 모바일 앱 필수 파일(아이콘, 매니페스트) 추가
 const ASSETS = [
-  './', 
-  './index.html', 
-  './transport_manifest.json',
-  './icons/icon.png',
-  './icons/icon.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).then(res => {
